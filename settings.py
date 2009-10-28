79005a178a7c13507ea6f8290a64ed6886f4c95d
stillflame <stillflame@shmutz.(none)>
2009-10-28 13:41:59 -0700

    - silly comments

--- settings.py
+++ settings.py
@@ -23,9 +23,6 @@ MANAGERS = ADMINS
 # system time zone.
 TIME_ZONE = 'Europe/London GB GB-Eire'
 
-#BASE_DIR="/home/mayera/Projects/Hongwu/human"
-#BASE_DIR="c:/human"
-
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
@@ -39,7 +36,7 @@ GRAPH_DIR="/tmp/graphs/"
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-#STATIC_PATH = 'C:/python25/lib/site-packages/django/human/maps'
+
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = BASE_REL('media')
@@ -48,7 +45,6 @@ CANVIZGRAPHPATH = os.path.join(MEDIA_ROOT, "nets", "livenet.xdot")
 
 #/path/to/media to use for static files like css; using the same path as 
 #ADMIN_MEDIA_PREFIX (which defaults to /media/) will overwrite URLconf entry 
-#STATIC_DOC_ROOT = '/home/mayera/Projects/Hongwu/human/nets/media' 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
